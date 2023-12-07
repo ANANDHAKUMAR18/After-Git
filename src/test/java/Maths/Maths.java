c7e88d1994c7729848963527bb4fb8cc5a771416
anandhakumarg <anandhsandy.g@gmail.com>
2023-12-08 05:10:56 +0530

    This is First Commit for today

---
+++ src/test/java/Maths/Maths.java
@@ -0,0 +1,17 @@
+package Maths;
+
+import Maths.Maths;
+
+public class Maths {
+	
+	public void add(int a,int b){
+		int result =a+b;
+		System.out.println("Sum of two numbers =" + result);
+		}
+	
+	
+	public static void main(String[] args){
+    Maths obj=new Maths();
+	obj.add(3,0);
+	}
+}
